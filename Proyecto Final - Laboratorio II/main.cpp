d7c0e8844e71d4c322627077487d0783ea009faa
LeandroVillada <leandro.villada.n@gmail.com>
2022-03-20 23:32:42 -0300

    Agregando clases y funciones
    Funciones agregada:
    cargarCadenas(te permite cargar palabras con espacios);
    cargar empleados;
    mostrar empleados;
    crear archivo .dat de empleados;
    Clases agregadas;
    Direccion;
    Fecha;
    Empleados; Funciones:
    Manejos de Archivos;
    Buscar por legajo;
    dar de baja por legajo;
    comprobar si ya existe el legajo;

--- Proyecto Final - Laboratorio II/main.cpp
+++ Proyecto Final - Laboratorio II/main.cpp
@@ -6,26 +6,40 @@
 #include <cstdlib>
 using namespace std;
 
+#include "clases.h"
+#include "funciones.h"
+
 int main()
 {
-    system("color 6");
     // Variables
     int opc;
 
     // Desarrollo del Programa
     while (true)
     {
+        system("color 6");
         system("cls");
         cout << "MENU PRINCIPAL" << endl;
-        cout << "\n\t1. " << endl;
-        cout << "\t2. " << endl;
-        cout << "\nSELECCIONE UNA DE LAS OPCIONES:  ";
+        cout << "\n\t1. Cargar Empleado" << endl;
+        cout << "\t2. Mostrar Empleados" << endl;
+        cout << "\t3. Crear Archivo Nuevo de Empleados" << endl;
+        cout << "\t0. Salir del Programa" << endl;
+        cout << "\nSELECCIONE UNA OPCION:  ";
         cin >> opc;
         system("cls");
         switch (opc)
         {
         case 1:
-            system("pasuse>nul");
+            cargarEmpleado();
+            system("pause>nul");
+            break;
+        case 2:
+            mostrarEmpleados();
+            system("pause>nul");
+            break;
+        case 3:
+            crearArchivoEmpleadoNuevo();
+            system("pause>nul");
             break;
         case 0: // Fin del Programa
             return 0;
