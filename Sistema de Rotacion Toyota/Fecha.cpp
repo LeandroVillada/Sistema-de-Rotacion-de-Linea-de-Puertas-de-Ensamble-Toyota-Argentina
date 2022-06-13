380eabb8c98043e4360d6abe64af5c0e1a05372d
LeandroVillada <leandro.villada.n@gmail.com>
2022-06-12 21:52:07 -0300

    Cambios en la clase Fecha

--- Sistema de Rotacion Toyota/Fecha.cpp
+++ Sistema de Rotacion Toyota/Fecha.cpp
@@ -1,5 +1,4 @@
 #include "Fecha.h"
-#include <iostream>
 // COSNTRUCTORES
 Fecha::Fecha()
 {
@@ -8,15 +7,51 @@ Fecha::Fecha()
     _anio = now->tm_year + 1900;
     _mes = now->tm_mon + 1;
     _dia = now->tm_mday;
-    _dia_maximo = 0;
 }
 Fecha::Fecha(int dia, int mes, int anio)
 {
     _dia = dia;
     _mes = mes;
     _anio = anio;
-    _dia_maximo = 0;
 }
+// FUNCIONES PRIVADAS
+bool Fecha::esbisiesto(int a)
+{
+    bool bisiesto = false;
+    if (a % 4 == 0)
+    {
+        bisiesto = true;
+    }
+    if ((a % 100 == 0) && (a % 400 != 0))
+    {
+        bisiesto = false;
+    }
+    return bisiesto;
+}
+void Fecha::NumeroSerieaFecha(int ns)
+{
+    int suma = 0, dia_adicional, suma_ant{};
+    int anio_real = 1900;
+    while (ns > suma)
+    {
+        dia_adicional = (esbisiesto(anio_real) ? 1 : 0);
+        suma_ant = suma;
+        suma += 365 + dia_adicional;
+        anio_real++;
+    }
+    _anio = anio_real - 1;
+    // Cantidad de dias pasados desde anio
+    int dias_anio_actual = ns - suma_ant, i = 0;
+    int vmes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (esbisiesto(_anio))
+        vmes[1] = 29;
+    suma = 0;
+    while (dias_anio_actual > suma)
+        suma += vmes[i++];
+    _mes = i;
+    _dia = dias_anio_actual - (suma - vmes[i - 1]);
+}
+
 // SETTERS
 void Fecha::setDia(int d) { _dia = d; }
 void Fecha::setMes(int m) { _mes = m; }
@@ -72,15 +107,111 @@ bool Fecha::operator<(Fecha aux)
         return false;
     return true;
 }
-// FUNCIONES
-bool Fecha::Cargar()
+int Fecha::operator==(const char *diaSemana)
+{
+    int cantdias = NumeroSerie() - 1; // cantidad de dias transcurridos desde 01/01/1900
+    // El 01/01/1900 fue Lunes
+    int numerodia = cantdias % 7;
+
+    char mdia[7][10];
+    strcpy(mdia[0], "Lunes");
+    strcpy(mdia[1], "Martes");
+    strcpy(mdia[2], "Miercoles");
+    strcpy(mdia[3], "Jueves");
+    strcpy(mdia[4], "Viernes");
+    strcpy(mdia[5], "Sabado");
+    strcpy(mdia[6], "Domingo");
+    if (strcmp(mdia[numerodia], diaSemana) == 0)
+        return 1;
+    return 0;
+}
+istream &operator>>(istream &entra, Fecha &f)
 {
     std::cout << "DIA: ";
-    std::cin >> _dia;
+    entra >> f._dia;
     std::cout << "MES: ";
-    std::cin >> _mes;
+    entra >> f._mes;
     std::cout << "ANIO: ";
-    std::cin >> _anio;
+    entra >> f._anio;
+    return entra;
+}
+ostream &operator<<(ostream &sale, Fecha &f)
+{
+    std::cout << "DIA: ";
+    sale << f._dia << endl;
+    std::cout << "MES: ";
+    sale << f._mes << endl;
+    std::cout << "ANIO: ";
+    sale << f._anio << endl;
+    return sale;
+}
+Fecha Fecha::operator+(int masdias)
+{
+  Fecha aux;
+  int ns = NumeroSerie() + masdias;
+  aux.NumeroSerieaFecha(ns);
+  return aux;
+}
+void Fecha::operator+=(int masdias)
+{
+  int ns = NumeroSerie() + masdias;
+  NumeroSerieaFecha(ns);
+}
+int Fecha::operator-(Fecha &obj)
+{
+  return NumeroSerie() - obj.NumeroSerie();
+}
+int Fecha::cantDiasMes()
+{
+  int vmes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  if (esbisiesto(_anio))
+    vmes[1] = 29;
+  return vmes[_mes - 1];
+}
+
+
+// FUNCIONES
+int Fecha::NumeroSerie() // Devuelve la cantidad de dias transcurridos desde el 01/01/1900
+{
+    int cont = 0, i;
+    for (i = 1900; i < _anio; i++)
+        if (esbisiesto(i))
+            cont++;
+    int vmes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}, suma = 0;
+    if (esbisiesto(_anio))
+        vmes[1] = 29;
+    for (int i = 0; i < _mes - 1; i++)
+        suma += vmes[i];
+    int cantdias = (_anio - 1900) * 365 + cont + suma + _dia;
+    return cantdias;
+}
+int Fecha::cantDiasHabilesMes()
+{
+  int totDias = cantDiasMes();
+  int dia, cant = 0;
+  Fecha aux = *this;
+  for (int i = 1; i <= totDias; i++)
+  {
+    dia = (aux.NumeroSerie() - 1) % 7;
+    if (dia < 5)
+      cant++;
+    aux += 1;
+  }
+  return cant;
+}
+int Fecha::cantAniosHastaHoy()
+{
+  Fecha aux; // por usar un constructor por defecto tiene la fecha de hoy;
+  int cant = aux._anio - _anio;
+  if (_mes > aux._mes)
+    return cant - 1;
+  if (_mes == aux._mes && _dia > aux._dia)
+    return cant - 1;
+  return cant;
+}
+bool Fecha::validarFecha()
+{
+    int dia_maximo = 0;
     if (_mes >= 1 && _mes <= 12)
     {
         switch (_mes)
@@ -92,33 +223,65 @@ bool Fecha::Cargar()
         case 8:
         case 10:
         case 12:
-            _dia_maximo = 31;
+            dia_maximo = 31;
             break;
         case 4:
         case 6:
         case 9:
         case 11:
-            _dia_maximo = 30;
+            dia_maximo = 30;
             break;
         case 2:
             if (((_anio % 4 == 0) && (_anio % 100 != 0)) || (_anio % 400 == 0))
             {
-                _dia_maximo = 29;
+                dia_maximo = 29;
             }
             else
             {
-                _dia_maximo = 28;
+                dia_maximo = 28;
             }
             break;
         }
-        if (_dia >= 1 && _dia <= _dia_maximo)
+        if (_dia >= 1 && _dia <= dia_maximo)
         {
             return true;
         }
     }
     return false;
 }
-void Fecha::Mostrar()
+void Fecha::MostrarFecha()
 {
     std::cout << _dia << "/" << _mes << "/" << _anio << std::endl;
+}
+void Fecha::MostrarFechaTexto()
+{
+    char mmes[12][11];
+    strcpy(mmes[0], "Enero");
+    strcpy(mmes[1], "Febrero");
+    strcpy(mmes[2], "Marzo");
+    strcpy(mmes[3], "Abril");
+    strcpy(mmes[4], "Mayo");
+    strcpy(mmes[5], "Junio");
+    strcpy(mmes[6], "Julio");
+    strcpy(mmes[7], "Agosto");
+    strcpy(mmes[8], "Septiembre");
+    strcpy(mmes[9], "Octubre");
+    strcpy(mmes[10], "Noviembre");
+    strcpy(mmes[11], "Diciembre");
+
+    int cantdias = NumeroSerie() - 1; // cantidad de dias transcurridos desde 01/01/1900
+    // El 01/01/1900 fue Lunes
+    int numerodia = cantdias % 7;
+
+    char mdia[7][10];
+    strcpy(mdia[0], "Lunes");
+    strcpy(mdia[1], "Martes");
+    strcpy(mdia[2], "Miercoles");
+    strcpy(mdia[3], "Jueves");
+    strcpy(mdia[4], "Viernes");
+    strcpy(mdia[5], "Sabado");
+    strcpy(mdia[6], "Domingo");
+
+    cout << endl
+         << mdia[numerodia] << " " << _dia << " de " << mmes[_mes - 1] << " de " << _anio << endl;
 }
--- Sistema de Rotacion Toyota/Fecha.h
+++ Sistema de Rotacion Toyota/Fecha.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <ctime>
+#include <iostream>
+#include <cstring>
+using namespace std;
 
 class Fecha
 {
@@ -7,7 +10,8 @@ private:
     int _dia;
     int _mes;
     int _anio;
-    int _dia_maximo;
+    bool esbisiesto(int);
+    void NumeroSerieaFecha(int);
 
 public:
     Fecha();
@@ -26,7 +30,19 @@ public:
     bool operator!=(Fecha);
     bool operator>(Fecha);
     bool operator<(Fecha);
+    Fecha operator+(int);
+    int operator -(Fecha &);
+    void operator +=(int);
+    int operator==(const char*);
+    friend istream & operator>>(istream &, Fecha &);
+    friend ostream & operator<<(ostream &, Fecha &);
     // FUNCIONES
+    int cantDiasMes();
+    int cantDiasHabilesMes();
+    int cantAniosHastaHoy();
+    int NumeroSerie();
+    void MostrarFecha();
+    void MostrarFechaTexto();
+    bool validarFecha();
     bool Cargar();
-    void Mostrar();
 };
